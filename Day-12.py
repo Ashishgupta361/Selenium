f10ee8a30101471028b41a490b7596605e1947b9
Ashishgupta361 <ashishjhi361@gmail.com>
2025-02-13 23:06:37 +0530

    Action chains code for handling advanced user interactions

---
+++ Day-12.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.implicitly_wait(5)
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+action = ActionChains(driver)
+
+#action.double_click()
+#action.context_click()
+#action.drag_and_drop()
+action.move_to_element(driver.find_element(By.ID,"mousehover")).perform()
+#action.context_click(driver.find_element(By.LINK_TEXT,"Top")).perform()
+action.context_click(driver.find_element(By.LINK_TEXT,"Reload")).click().perform()
