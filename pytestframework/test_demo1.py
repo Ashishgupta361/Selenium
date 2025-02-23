f26e95c8ad140567fb57507f1c4c2f7fb5c6b821
Ashishgupta361 <ashishjhi361@gmail.com>
2025-02-23 23:39:20 +0530

    Updated code of pytest implementation

--- pytestframework/test_demo1.py
+++ pytestframework/test_demo1.py
@@ -1,5 +1,23 @@
 # Any pytest file should start with test_
 # method name should start with test
 # All code should be wrapped inside methos only
+# To run whole framework command => py.test
+# -v -> verbose i.e. more information
+# -s -> to show all console logs
+
+
+# def test_firstProgram():
+#     print("Hello")
+#
+# def test_firstProgram():
+#     print("Good morning")
+
+# if we use same name for two methods then last method will be get printed in output
+# because earlier methods are over-ridden.
+
+
 def test_firstProgram():
     print("Hello")
+
+def test_Greet():
+    print("Good morning")
