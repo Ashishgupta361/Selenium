ee59e13deb75902c3bdb603470ed80e6bf5fafb9
Ashishgupta361 <ashishjhi361@gmail.com>
2025-02-24 22:26:26 +0530

    pytest

--- pytestframework/test_demo2.py
+++ pytestframework/test_demo2.py
@@ -1,5 +1,14 @@
-
+# For running specific file => py.test filename -v -s
+# method name should have sense
+# -k stands for method name execution
+#  => py.test -k <method name> -v -s
 
 def test_firstProgram():
     msg = "Hello"
     assert msg == "Hi","Test Failed as strings do not match"
+
+
+def test_addcreditamount():
+    a = 4
+    b = 6
+    assert a+2 == b, "Test Failed as addition do not match"
