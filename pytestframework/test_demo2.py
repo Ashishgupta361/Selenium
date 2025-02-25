f45216e4852eb5191d74ae88f6f3d689a47098b5
Ashishgupta361 <ashishjhi361@gmail.com>
2025-02-25 22:33:38 +0530

    Added code for grouping/mark tests

--- pytestframework/test_demo2.py
+++ pytestframework/test_demo2.py
@@ -2,12 +2,17 @@
 # method name should have sense
 # -k stands for method name execution
 #  => py.test -k <method name> -v -s
+# you can mark (tag) tests @pytest.mark.smoke and run with -m => py.test -m smoke -v -s
+# you can skip tests with @pytest.mark.skip => py.test -v -s
+import pytest
 
+
+@pytest.mark.smoke
 def test_firstProgram():
     msg = "Hello"
     assert msg == "Hi","Test Failed as strings do not match"
 
-
+@pytest.mark.skip
 def test_addcreditamount():
     a = 4
     b = 6
