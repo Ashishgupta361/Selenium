dfcaef9dc2fdae7d6d7cf5e4be5bf712a8b58350
Ashishgupta361 <ashishjhi361@gmail.com>
2025-04-08 21:51:39 +0530

    Added: fixture that returns data'

--- pytestframework/conftest.py
+++ pytestframework/conftest.py
@@ -1,7 +1,15 @@
 import pytest
 
+# If we want to run fixture only once then pass parameter in fixture() as scope="class"
+# @pytest.fixture(scope="class")
 @pytest.fixture()
+
 def setup():
     print("I will be executed first - setup")
     yield
     print("I will executed last")
+
+@pytest.fixture()
+def dataload():
+    print("user profile data is being created")
+    return ["Rahul","Shetty","rahulshettyacademy.com"]
